b25a89a38ae3681c78c2dc8785212424f877aedd
m-mitoma <mitoma2530@yahoo.co.jp>
2025-06-01 12:00:17 +0900

    githubpages用の設定変更、パスをサブディレクトリ対応可能に変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { ROUTES } from './const'
-import HomePage from './pages/HomePage'
-import ResultPage from './pages/ResultPage'
-import QuizPage from './pages/QuizPage'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { ROUTES } from "./const";
+import HomePage from "./pages/HomePage";
+import ResultPage from "./pages/ResultPage";
+import QuizPage from "./pages/QuizPage";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<HomePage />} />
-        <Route path={ROUTES.QUIZ} element={<QuizPage />} />
-        <Route path={ROUTES.RESULT} element={<ResultPage />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Routes>
+      <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.QUIZ} element={<QuizPage />} />
+      <Route path={ROUTES.RESULT} element={<ResultPage />} />
+    </Routes>
+  );
 }
-export default App
+export default App;
